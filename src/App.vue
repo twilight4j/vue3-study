d5fe2ab118a826221f34ad6ae70fad68a9abc340
1327472 <jaeyulim@lotte.net>
2022-11-16 15:43:34 +0900

    프로젝트 생성

---
+++ src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <button @click="add">
+    ADD
+  </button>
+  <h1>{{ reversedMessage }}</h1>
+  <h1>{{ reversedMessage }}</h1>
+  <h1>{{ reversedMessage }}</h1>
+  <h1>{{ reversedMessage }}</h1>
+</template>
+
+
+<script>
+export default {
+  data() {
+    return {
+      // Getter, Setter
+      msg : 'Helo Computed!'
+    }
+  },
+  computed: {
+    // Getter
+    // reversedMessage() {
+    //   return this.msg.split('').reverse().join('')
+    // }
+    // Getter, Setter
+    reversedMessage: {
+      get() {
+       return this.msg.split('').reverse().join('') 
+      },
+      set(value) {
+        this.msg = value
+      }
+    }
+  },
+  methods: {
+    add() {
+      this.reversedMessage += '!?'
+    }
+  }
+}</script>
---
+++ src/components/Fruits.vue
@@ -0,0 +1,44 @@
+<template>
+  <section v-if="hasFruit">
+    <h1>Fruits</h1>
+    <ul>
+      <li
+        v-for="fruit in fruits"
+        :key="fruit">
+        {{ fruit }}
+      </li>
+    </ul>
+  </section>
+  <section>
+    <h1>Reverse Fruits</h1>
+    <ul>
+      <li
+        v-for="fruit in reverseFruits"
+        :key="fruit">
+        {{ fruit }}
+      </li>
+    </ul>
+  </section>
+</template> 
+
+<script>
+export default {
+  data() {
+    return {
+      fruits: [
+        'Appple', 'Banana', 'Cherry'
+      ]
+    }
+  },
+  computed: {
+    hasFruit() {
+      return this.fruits.length > 0
+    },
+    reverseFruits(){
+      return this.fruits.map(fruit => {
+        return fruit.split('').reverse().join('')
+      })
+    }
+  }
+}
+</script>
