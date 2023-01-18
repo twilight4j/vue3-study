2ea02651380bfd066acab04ca57a8f6ac6ec8794
twilight4j <twilight4j@gmail.com>
2023-01-18 23:12:24 +0900

    20.컴포넌트-Refs

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,15 @@
 <template>
-  <button @click="message = 'Good?'">
-    Click!
-  </button>
-  <h1>App: {{ message }}</h1>
-  <Parent />
+  <Hello ref="hello" />
 </template>
 
 <script>
-import Parent from '~/components/Parent'
-import { computed } from 'vue'
-
+import Hello from '~/components/Hello'
 export default {
   components: {
-    Parent
+    Hello
   },
-  data() {
-    return {
-      message: 'Hello world!'
-    }
-  },
-  provide() {
-    return {
-      msg: computed(()=>{
-        return this.message
-      })
-    }
+  mounted() {
+    console.log('mounted:', this.$refs.hello.$refs.good)
   }
 }
 </script>
